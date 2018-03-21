6a1df65fc2b8e977613e40141b0e18b32d607908
NguyenVanBay <nguyenvanbay.no1@gmail.com>
2018-03-21 22:21:30 +0700

    edit
    Tóm tắt code.

--- js/jquery.inner_float.js
+++ js/jquery.inner_float.js
@@ -26,24 +26,32 @@ $(function() {
 				var heightWindow = parseInt($(this).scrollTop());
 
 				if((parseInt(heightTop) - parseInt(heightWindow)) > parseInt(setting.top)) {
+
 					thisCallfn.removeClass("fixed");
+
 				} else {
-					thisCallfn.addClass("fixed");
-					thisCallfn.attr("top", "50px");
 
 					var h1 = parseInt(heightWindow);
 					var h2 = parseInt(thisCallfn.parent().height()) + parseInt(heightTop)  - parseInt(setting.top) - parseInt(thisCallfn.height());
 					// console.log(h1);
 					// console.log(h2);
 
-					if(h1 > h2) {
+					if(h1 < h2) {
+
+						// console.log("Chưa tới");
+						thisCallfn.removeClass("absolute");
+						thisCallfn.removeAttr('style');
+						thisCallfn.addClass("fixed");
+						thisCallfn.css("top", setting.top + "px");
+
+					} else {
+
 						// console.log("qua");
 						thisCallfn.removeClass("fixed");
+						thisCallfn.removeAttr('style');
 						thisCallfn.addClass("absolute");
-					} else {
-						// console.log("DDax toiws");
-						thisCallfn.addClass("fixed");
-						thisCallfn.removeClass("absolute");
+						thisCallfn.css("bottom", "0px");
+
 					}
 				}
 			});
