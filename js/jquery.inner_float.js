b2651fabf842f8b18f6510b8605f36c4f07e935c
NguyenVanBay <nguyenvanbay.no1@gmail.com>
2018-03-22 09:50:55 +0700

    hoan thanh
    hoanthanh

--- js/jquery.inner_float.js
+++ js/jquery.inner_float.js
@@ -17,40 +17,53 @@ $(function() {
 				heightTop += parseInt(this.offsetHeight);
 			});
 
-			thisCallfn.parent().addClass("relative");
-		
+			thisCallfn.addClass("th1");
+
 			// sự kiện khi scroll màn hình.
 			$( window ).scroll (function() {
 
 				// lấy giá trị chuột cuộn.
 				var heightWindow = parseInt($(this).scrollTop());
 
-				if((parseInt(heightTop) - parseInt(heightWindow)) > parseInt(setting.top)) {
+				if((parseInt(heightTop) - parseInt(heightWindow) + 20) > parseInt(setting.top)) {
 
-					thisCallfn.removeClass("fixed");
+					// th1.
+					thisCallfn.removeClass();
+					thisCallfn.addClass("th1");
+					thisCallfn.addClass("bg-red");
 
 				} else {
 
 					var h1 = parseInt(heightWindow);
-					var h2 = parseInt(thisCallfn.parent().height()) + parseInt(heightTop)  - parseInt(setting.top) - parseInt(thisCallfn.height());
+					var h2 = parseInt(thisCallfn.parent().height()) + parseInt(heightTop)  - parseInt(setting.top) - parseInt(thisCallfn.height()) - 20;
 					// console.log(h1);
 					// console.log(h2);
 
 					if(h1 < h2) {
 
-						// console.log("Chưa tới");
-						thisCallfn.removeClass("absolute");
-						thisCallfn.removeAttr('style');
-						thisCallfn.addClass("fixed");
-						thisCallfn.css("top", setting.top + "px");
+						thisCallfn.removeClass();
+						thisCallfn.addClass("th2");
+						thisCallfn.addClass("bg-red");
+
+
+						// // console.log("Chưa tới");
+						// thisCallfn.removeClass("absolute");
+						// thisCallfn.removeAttr('style');
+						// thisCallfn.addClass("fixed");
+						// thisCallfn.css("top", setting.top + "px");
 
 					} else {
 
+						thisCallfn.removeClass();
+						thisCallfn.addClass("th3");
+						thisCallfn.addClass("bg-red");
+						// thisCallfn.removeClass("th2");
+						// thisCallfn.removeClass("th1");
 						// console.log("qua");
-						thisCallfn.removeClass("fixed");
-						thisCallfn.removeAttr('style');
-						thisCallfn.addClass("absolute");
-						thisCallfn.css("bottom", "0px");
+						// thisCallfn.removeClass("fixed");
+						// thisCallfn.removeAttr('style');
+						// thisCallfn.addClass("absolute");
+						// thisCallfn.css("bottom", "0px");
 
 					}
 				}
